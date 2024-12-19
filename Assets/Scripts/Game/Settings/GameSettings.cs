3cb43fdd4ed2d4c0741ed2811264f00de3dc385d
Alexey Vlasyuk <mail@i-alex.net>
2024-12-19 22:49:13 +0300

    Define a scriptable object for game settings

---
+++ Assets/Scripts/Game/Settings/GameSettings.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace Match3Test.Game.Settings
+{
+    [CreateAssetMenu(menuName = "Match3Test/GameSettings", fileName = "GameSettings")]
+    public class GameSettings : ScriptableObject
+    {
+        
+    }
+}
